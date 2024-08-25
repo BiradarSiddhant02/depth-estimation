5817f3d75b52ae112d0eed3c299ad52176793206
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-08-25 09:04:16 +0530

    remove autocast

--- inferencing/rpi_inference.py
+++ inferencing/rpi_inference.py
@@ -72,11 +72,10 @@ image_tensor = (
 
 print("Running inference...")
 # Inference
-with torch.cpu.amp.autocast(dtype=torch.float16):
-    with torch.no_grad():
-        start = time()
-        output_0 = depth_model(image_tensor)
-        end = time()
+with torch.no_grad():
+    start = time()
+    output_0 = depth_model(image_tensor)
+    end = time()
 
 print(f"Inference completed in {end - start:.2f} seconds.")
 
